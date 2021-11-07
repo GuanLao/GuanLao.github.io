af968e5c41208b44c4f23cf1919727b9b8803e0e
Guan Lao <hlao@ucdavis.edu>
2021-11-07 12:49:43 -0800

    newchange

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -9,15 +9,33 @@
         sections[0].className = "hidden";
         sections[1].className = "showing orange";
     });
-
-    document.querySelector('#gotoinput2').addEventListener('click', function() {
-        sections[1].className = "hidden";
-        sections[2].className = "showing magenta";
-    });
-
-    document.querySelector('#gotomadlib').addEventListener('click', function() {
-        sections[2].className = "hidden";
-        sections[3].className = "showing gray";
-    });
-
+    const scroll = window.requestAnimationFrame || function(callback){
+        window.setTimeout(callback,1000/60)};
+    const elementsToShow = document.querySelectorAll(".show-on-scroll");
+    
+    function loop(){
+        elementsToShow.forEach(function(element){
+        
+            if(isElementInViewport(element)){
+                element.classList.add("is-visible");
+            }else{
+                element.classList.remove("is-visible")
+            }
+        });
+        scroll(loop);
+            }
+            loop();
+    function isElementInViewport(el){
+        if(typeof jQuery === "function" && el instanceof jQuery){
+            el=el[0];
+        }
+        const rect = el.getBoundingClientRect();
+        return(rect.top <= 0 && rect.bottom >=0)
+        || 
+        (rect.bottom>= (window.innerHeight|| document.documentElement.clientHeight)&& 
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    }
 })();
